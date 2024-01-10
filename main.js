fdeb37846b43b3c7b583ade02f84a8419b5d01c4
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-10 16:49:43 +0100

    feat immagini dadi

--- main.js
+++ main.js
@@ -4,11 +4,27 @@ const playerTwo = document.getElementById("userTwo");
 function play(){
     let diceOne = Math.floor(Math.random()*6 +1);
     let diceTwo = Math.floor(Math.random()*6 +1);
-    playerOne.textContent = `Your roll is : ${diceOne} & ${diceTwo}`
+    let playerOneTotal = diceOne+diceTwo;
+    console.log(playerOneTotal);
+    playerOne.innerHTML = `Your roll is :<img src="./img/dice-six-faces-${diceOne}.svg">&nbsp;<img src="./img/dice-six-faces-${diceTwo}.svg">`
     let diceThree = Math.floor(Math.random()*6 +1);
     let diceFour = Math.floor(Math.random()*6 +1);
-    playerTwo.textContent = `Your roll is : ${diceThree} & ${diceFour}`
+    let playerTwoTotal = diceThree+diceFour;
+    console.log(playerTwoTotal);
+    playerTwo.innerHTML = `Your roll is :<img src="./img/dice-six-faces-${diceThree}.svg">&nbsp;<img src="./img/dice-six-faces-${diceFour}.svg">`
+    const vincitore = document.getElementById("winner");
+    
+    if (playerOneTotal > playerTwoTotal){
+        vincitore.innerHTML = "Plyer 1 wins!"
+    } else if (playerOneTotal < playerTwoTotal){
+        vincitore.innerHTML = "Plyer 2 wins!"
+    } else if (playerOneTotal === playerTwoTotal){
+        vincitore.innerHTML = "It is a Tie!"
+    }
 }
+
+
+
 
 
 
